bb2e947e57366ca0427b7f2ef7322c415814c06e
Revinianz13 <kitesurf@windowslive.com>
2023-06-15 21:11:14 +0300

    Added some components

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,67 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource') " :mode="addResButtonMode">Add Resources</base-button>
+  </base-card>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {StoredResources, AddResource},
+  data () {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          tittle: 'Official Guide',
+          description: 'The official Vue.js Documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          tittle: 'Google',
+          description: 'Learn Googling',
+          link: 'https://google.com',
+        },
+      ],
+    }
+  },
+  provide () {
+    return {
+      resources: this.storedResources,
+      addResource :this.addResource
+    }
+  },
+  computed : {
+    storedResButtonMode () {
+      return this.selectedTab==='stored-resources' ? null : 'flat'
+    },
+    addResButtonMode () {
+      return this.selectedTab==='add-resource' ? null : 'flat'
+    }
+  },
+  methods:{
+    setSelectedTab (tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title,description,url) {
+      const newResource= {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link : url
+      }
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
+    }
+  }
+
+}
+</script>
